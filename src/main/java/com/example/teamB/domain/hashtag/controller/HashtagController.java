4fd2cb3e6a9a43c7919468aeaf20e2dfc806708b
Jung Hoon <52675944+JH627@users.noreply.github.com>
2024-11-18 19:28:45 +0900

    ♻️ refactor: CurrentUser 어노테이션 적용 #30

--- src/main/java/com/example/teamB/domain/hashtag/controller/HashtagController.java
+++ src/main/java/com/example/teamB/domain/hashtag/controller/HashtagController.java
@@ -3,8 +3,8 @@ package com.example.teamB.domain.hashtag.controller;
 import com.example.teamB.domain.hashtag.dto.HashtagResponseDTO;
 import com.example.teamB.domain.hashtag.service.query.HashtagQueryService;
 import com.example.teamB.domain.member.entity.Member;
-import com.example.teamB.domain.member.service.command.MemberCommandService;
 import com.example.teamB.global.apiPayload.CustomResponse;
+import com.example.teamB.global.jwt.annotation.CurrentMember;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -19,16 +19,13 @@ import org.springframework.web.bind.annotation.*;
 public class HashtagController {
 
     private final HashtagQueryService hashtagQueryService;
-    private final MemberCommandService memberCommandService;
 
     @GetMapping("/recommendation")
     @Operation(summary = "오늘의 옷(해시태그) 추천 조회", description = "해시태그와 각 해시태그에 맞는 옷 사진을 반환합니다.")
     public CustomResponse<HashtagResponseDTO.DailyRecommendationListDTO> getRecommendation (
-            @RequestHeader("Authorization") String authorizationHeader,
+            @CurrentMember Member member,
             @RequestParam(value = "maxTemperature") int maxTemperature,
             @RequestParam(value = "minTemperature") int minTemperature) {
-        String accessToken = authorizationHeader.replace("Bearer ", "");
-        Member member = memberCommandService.getMemberFromToken(accessToken);
         return CustomResponse.onSuccess(hashtagQueryService.getRecommendation(maxTemperature, minTemperature));
     }
 }
