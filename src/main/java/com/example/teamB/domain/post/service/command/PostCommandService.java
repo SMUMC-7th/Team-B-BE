5e76fc4dc6db092268d4362847d62cf9a44a1a28
leeeemj <leemj2944@gmail.com>
2024-12-16 21:01:49 +0900

    refactor:post controller

--- src/main/java/com/example/teamB/domain/post/service/command/PostCommandService.java
+++ src/main/java/com/example/teamB/domain/post/service/command/PostCommandService.java
@@ -1,9 +1,11 @@
 package com.example.teamB.domain.post.service.command;
 
 import com.example.teamB.domain.post.dto.PostRequestDTO;
+import com.example.teamB.domain.post.dto.PostResponseDTO;
+import com.example.teamB.domain.post.entity.Post;
 
 public interface PostCommandService {
     Long createPost(PostRequestDTO.CreatePostDTO dto, Long memberId);
-    void updatePost(PostRequestDTO.CreatePostDTO dto, Long memberId, Long postId);
+    Post updatePost(PostRequestDTO.UpdatePostDTO dto, Long memberId, Long postId);
     void deletePost(Long postId, Long memberId);
 }
